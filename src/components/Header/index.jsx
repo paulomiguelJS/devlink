c9ae9690c27d9f830b3fde834ee67b54bcd8b565
paulomigueljs <paulomiguelblues@gmail.com>
2022-11-10 21:30:57 -0500

    Signout implemented and tested

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,14 @@ import { Container } from "./styles";
 import { BiLogOut } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../services/firebaseConnection";
+import { signOut } from "firebase/auth";
+
 export function Header() {
   async function handleLogout() {
-    alert("test");
+    await signOut(auth);
   }
+
   return (
     <Container>
       <nav>
