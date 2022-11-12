33b8823ee80b50248498353b407571517963f5de
paulomigueljs <paulomiguelblues@gmail.com>
2022-11-11 21:40:57 -0500

    Preview and inputs values captured

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,37 +1,82 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
 import { Logo } from "../../components/Logo";
 import { Input } from "../../components/Input";
 
 import { MdAddLink } from "react-icons/md";
+import { FiTrash2 } from "react-icons/fi";
 
-import { Container, FormContainer } from "./styles";
+import { Container, FormContainer, Preview } from "./styles";
 
 export function Admin() {
+  const [nameInput, setNameInput] = useState("");
+  const [urlInput, setUrlInput] = useState("");
+  const [backgroundColorInput, setBackgroundColorInput] = useState("#f1f1f1");
+  const [textColorInput, setTextColorInput] = useState("#121212");
+
   return (
     <Container>
       <Header />
       <Logo />
       <FormContainer>
         <label>Link Name</label>
-        <Input placeholder="Link Name" />
+        <Input
+          placeholder="Link Name"
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+        />
         <label>URL link</label>
-        <Input placeholder="URL link" />
-        <label>Link Name</label>
-        <Input placeholder="Link Name" />
+        <Input
+          placeholder="URL link"
+          value={urlInput}
+          onChange={(e) => setUrlInput(e.target.value)}
+        />
+
         <section>
           <div>
             <label>Background link</label>
-            <input type="color" />
+            <input
+              type="color"
+              value={backgroundColorInput}
+              onChange={(e) => setBackgroundColorInput(e.target.value)}
+            />
           </div>
           <div>
             <label>Color link</label>
-            <input type="color" />
+            <input
+              type="color"
+              value={textColorInput}
+              onChange={(e) => setTextColorInput(e.target.value)}
+            />
           </div>
         </section>
+
+        {nameInput !== "" && (
+          <Preview>
+            <label> See how it looks like </label>
+            <article style={{ backgroundColor: backgroundColorInput }}>
+              <p style={{ color: textColorInput }}>{nameInput}</p>
+            </article>
+          </Preview>
+        )}
         <button type="submit">
           Add new link <MdAddLink size={24} color="#fff" />
         </button>
       </FormContainer>
+
+      <h2>My Links</h2>
+
+      <article
+        className="animate-pop"
+        style={{ backgroundColor: "#000", color: "#fff" }}
+      >
+        <p>Telegram</p>
+        <div>
+          <button>
+            <FiTrash2 size={18} color="#fff" />
+          </button>
+        </div>
+      </article>
     </Container>
   );
 }
--- src/pages/Admin/styles.js
+++ src/pages/Admin/styles.js
@@ -5,6 +5,46 @@ export const Container = styled.div`
   align-items: center;
   flex-direction: column;
   min-height: 100vh;
+
+  h2 {
+    color: #fff;
+    margin: 2rem 0 1rem 0F;
+  }
+
+  article {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 80%;
+    max-width: 600px;
+    background-color: #423423;
+    border-radius: 4px;
+    padding: 0.5rem 0.5rem;
+  }
+
+  button {
+    border: 1.5px dashed #fff;
+    color: #fff;
+    background-color: #000;
+    padding: 4px 8px;
+    border-radius: 4px;
+  }
+
+  .animate-pop {
+    animation-duration: 0.5s;
+    animation-name: animate-pop;
+  }
+
+  @keyframes animate-pop {
+    0% {
+      opacity: 0;
+      transform: scale(0.7, 0.7);
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1, 1);
+    }
+  }
 `;
 
 export const FormContainer = styled.form`
@@ -41,9 +81,25 @@ export const FormContainer = styled.form`
     border-radius: 4px;
     border: #3267ff;
     height: 2rem;
-    
+
     svg {
       margin-left: 1rem;
     }
+  }
+`;
+
+export const Preview = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  border: 1.5px solid rgba(255, 255, 255, 0.5);
+  padding-bottom: 1rem;
+  border-radius: 4px;
+
+  label {
+    color: #fff;
+    font-weight: medium;
+    margin-bottom: 1rem;
   }
 `;
