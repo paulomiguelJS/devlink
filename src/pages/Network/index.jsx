59d8e61fb12e69fc4530c05fa805b8dbdbb42b8c
paulomigueljs <paulomiguelblues@gmail.com>
2022-11-13 19:44:05 -0500

    Saved social medias in DB configured and toast added

--- src/pages/Network/index.jsx
+++ src/pages/Network/index.jsx
@@ -1,23 +1,54 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Container } from "./styles";
 import { MdAddLink } from "react-icons/md";
+import { toast } from "react-toastify";
+
+import { db } from "../../services/firebaseConnection";
+import { setDoc, doc, getDoc } from "firebase/firestore";
 
 export function Network() {
   const [facebook, setFacebook] = useState("");
   const [instagram, setInstagram] = useState("");
   const [youtube, setYoutube] = useState("");
 
-  async function handleSalve(e) {
-    alert("Teste")
+  useEffect(() => {
+    function loadLinks() {
+      const docRef = doc(db, "social", "link");
+      getDoc(docRef).then((snapshot) => {
+        if (snapshot.data() !== undefined) {
+          setFacebook(snapshot.data().facebook);
+          setInstagram(snapshot.data().instagram);
+          setYoutube(snapshot.data().youtube);
+        }
+      });
+    }
+    loadLinks();
+  }, []);
+
+  function handleSave(e) {
+    e.preventDefault();
+    setDoc(doc(db, "social", "link"), {
+      facebook: facebook,
+      instagram: instagram,
+      youtube: youtube,
+    })
+      .then(() => {
+        toast.success("The links have been saved");
+        console.log("Success");
+      })
+      .catch((error) => {
+        toast.error("Error, the links haven't saved");
+        console.log("Error" + error);
+      });
   }
 
   return (
     <Container>
       <Header />
       <h1>Your social medias</h1>
-      <form onSubmit={handleSalve}>
+      <form onSubmit={handleSave}>
         <label>Facebook link</label>
         <Input
           placeholder="Type your facebook url"
